15848b1c57152aadc8d5efa3061093ba493b9e28
Jake Leuthardt <jakeleuthardt1@gmail.com>
2025-07-08 21:56:01 -0500

    turn off no unescaped entities

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -90,6 +90,7 @@ export default defineConfig([globalIgnores([
         "react/prop-types": "off",
         "react/jsx-uses-react": "off",
         "react/react-in-jsx-scope": "off",
+        "react/no-unescaped-entities": "off",
         "react-hooks/exhaustive-deps": "off",
         "jsx-a11y/click-events-have-key-events": "warn",
         "jsx-a11y/interactive-supports-focus": "warn",
